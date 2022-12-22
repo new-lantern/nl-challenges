cb9f9e54f3ecf544b731de3b361869c7f256bfde
kevinshen1101 <40150857+kevinshen1101@users.noreply.github.com>
2022-12-21 17:54:33 -0800

    date filter

--- user_analytics/user_analytics/core/filters.py
+++ user_analytics/user_analytics/core/filters.py
@@ -19,9 +19,24 @@ class StudyFilter(django_filters.FilterSet):
         field_name="sla", lookup_expr="lte"
     )
 
+    date = django_filters.DateFilter()
+    date__gt = django_filters.DateFilter(
+        field_name="date", lookup_expr="gt"
+    )
+    date__lt = django_filters.DateFilter(
+        field_name="date", lookup_expr="lt"
+    )
+    date__gte = django_filters.DateFilter(
+        field_name="date", lookup_expr="gte"
+    )
+    date__lte = django_filters.DateFilter(
+        field_name="date", lookup_expr="lte"
+    )
+
+
     ordering = django_filters.OrderingFilter(
         fields=(
-            ("timestamp", "timestamp"),
+            ("date", "date"),
             ("modality", "modality"),
             ("body_part", "body_part"),
             ("id", "id"),
